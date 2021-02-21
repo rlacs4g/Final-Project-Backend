08c8b747b20c71c3c20333bcd657c0ab524ca12e
rlacs4g <lacson.rafael@gmail.com>
2021-02-21 23:15:35 +0000

    finished routes except private

--- src/main.py
+++ src/main.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, Day, Food
 #from models import Person
 from flask_jwt_extended import create_access_token
 from flask_jwt_extended import get_jwt_identity
@@ -38,15 +38,6 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
-@app.route('/user', methods=['GET'])
-def handle_hello():
-
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
-
-    return jsonify(response_body), 200
-
 @app.route('/register', methods=['POST'])
 def register():
     email = request.json.get("email", None)
@@ -54,6 +45,8 @@ def register():
     fname = request.json.get("fname", None)
     lname = request.json.get("lname", None)
     tos = request.json.get("tos", None) 
+
+
 
     if not email:
         return jsonify({"msg": "Email is required"}), 400
@@ -123,6 +116,203 @@ def login():
     
     # general return in json with status
     return jsonify(response_msg), status_code
+
+@app.route('/diary', methods=['POST'])
+def createDiaryEntry():
+
+    # make sure request is a json
+    if not request.is_json:
+        return jsonify({"msg": "Missing JSON in request"}), 400
+
+    
+    foods = request.json.get("foods", None)
+    date = request.json.get("date", None)
+    user_id = request.json.get("user_id", None)
+
+    if not foods:
+        return jsonify({"msg": "Array of Foods is required"}), 400
+    if not date:
+        return jsonify({"msg": "Date is required"}), 400
+    if not user_id:
+        return jsonify({"msg": "User ID is required"}), 400
+
+    user = User.query.filter_by(id=user_id).first()
+    if not user:
+        return jsonify({"msg": "Invalid User ID"}), 400 
+    
+    day = Day.query.filter_by(date=date).first()
+    if day is not None:
+        return jsonify({"msg": "Day already exists. Please update existing day instead of creating again."}), 400 
+    
+    try:
+        day = Day(date=date,user_id=user_id)
+        db.session.add(day)
+        db.session.commit()
+    except:
+        db.session.rollback()
+        return jsonify({"msg": "Failed to create day in journal"}), 400 
+
+    try:
+        for item in foods:
+            name = item.get("name", None)
+            calories = item.get("calories", None)
+            serving_size = item.get("serving_size", None)
+            serving_unit = item.get("serving_unit", None)
+            quantity = item.get("quantity", None)
+            time_of_day = item.get("time_of_day", None)
+            if not name:
+                return jsonify({"msg": "name is required"}), 400
+            if not quantity:
+                return jsonify({"msg": "quantity is required"}), 400
+            if not serving_size:
+                return jsonify({"msg": "serving size is required"}), 400
+            if not serving_unit:
+                return jsonify({"msg": "serving unit is required"}), 400
+            if not calories:
+                return jsonify({"msg": "calories is required"}), 400
+            if not time_of_day:
+                return jsonify({"msg": "time of day is required"}), 400     
+            
+            try:
+                food = Food(name=name, calories=calories, serving_size=serving_size, serving_unit=serving_unit, quantity=quantity, time_of_day=time_of_day, day_id=day.id)
+                print(food)
+                print(day.id)
+                db.session.add(food)
+                db.session.commit()
+            except:
+                db.session.rollback()  
+                return jsonify({"msg": "Failed to create food and associate to day"}), 400       
+        
+
+    except:
+        pass  
+
+
+    return jsonify({"msg": "Journal entry was recorded", "journal": day.serialize()}), 200
+
+@app.route('/diary/food', methods=['POST'])
+def createNewFood():
+    # make sure request is a json
+    if not request.is_json:
+        return jsonify({"msg": "Missing JSON in request"}), 400
+        
+    name = request.json.get("name", None)
+    calories = request.json.get("calories", None)
+    serving_size = request.json.get("serving_size", None)
+    serving_unit = request.json.get("serving_unit", None)
+    quantity = request.json.get("quantity", None)
+    time_of_day = request.json.get("time_of_day", None)    
+    day_id = request.json.get("day_id", None)    
+
+    if not name:
+        return jsonify({"msg": "name is required"}), 400
+    if not quantity:
+        return jsonify({"msg": "quantity is required"}), 400
+    if not serving_size:
+        return jsonify({"msg": "serving size is required"}), 400
+    if not serving_unit:
+        return jsonify({"msg": "serving unit is required"}), 400
+    if not calories:
+        return jsonify({"msg": "calories is required"}), 400
+    if not time_of_day:
+        return jsonify({"msg": "time of day is required"}), 400    
+    if not day_id:
+        return jsonify({"msg": "time of day is required"}), 400    
+    print(day_id)
+# json must have the food details and the id for the day that we are updating
+    food_entry = Day.query.filter_by(id = day_id).first()
+
+    if food_entry is None:
+        return jsonify({"msg": "The requested day does not exist. "}), 400
+
+
+    try:
+        food = Food(name=name, calories=calories, serving_size=serving_size, serving_unit=serving_unit, quantity=quantity, time_of_day=time_of_day, day_id=day_id)
+        db.session.add(food)
+        db.session.commit()
+    except:
+        db.session.rollback()  
+        return jsonify({"msg": "Failed to create food and associate to day"}), 400     
+
+    return jsonify({"msg": "Success", "food": food.serialize()}), 400   
+
+@app.route('/diary/food/<int:food_id>', methods=['DELETE','PUT'])
+def handleFoodDelete(food_id):
+    if not food_id:
+        return jsonify({"msg": "food_id is required"}), 400
+
+    food = Food.query.filter_by(id = food_id).first()
+    if food is None:
+        return jsonify({"msg": "ID not found. The requested food does not exist. "}), 400
+
+    if request.method =="PUT":
+        if not request.is_json:
+            return jsonify({"msg": "Missing JSON in request"}), 400
+        
+        name = request.json.get("name", None)
+        calories = request.json.get("calories", None)
+        serving_size = request.json.get("serving_size", None)
+        serving_unit = request.json.get("serving_unit", None)
+        quantity = request.json.get("quantity", None)
+        time_of_day = request.json.get("time_of_day", None)
+
+        if not name:
+            return jsonify({"msg": "name is required"}), 400
+        if not quantity:
+            return jsonify({"msg": "quantity is required"}), 400
+        if not serving_size:
+            return jsonify({"msg": "serving size is required"}), 400
+        if not serving_unit:
+            return jsonify({"msg": "serving unit is required"}), 400
+        if not calories:
+            return jsonify({"msg": "calories is required"}), 400
+        if not time_of_day:
+            return jsonify({"msg": "time of day is required"}), 400   
+
+        times = ["morning","afternoon","evening"]
+        if not time_of_day in times:
+            return jsonify({"msg": "Time_of_day must be 'morning', 'afternoon', or 'night'."}), 400 
+
+        try:
+            # handle updating qty or other info on an existing food
+            food.name = name
+            food.calories = calories
+            food.serving_size = serving_size
+            food.serving_unit = serving_unit
+            food.quantity = quantity
+            food.time_of_day = time_of_day
+
+            db.session.commit()
+        except Exception as e:
+            return jsonify({"msg": str(e)})
+        
+        return jsonify({
+            "msg": "Food successfully updated",
+            "food": food.serialize()
+        }), 200
+
+
+    if request.method =="DELETE":
+        # handle deleting/removing food item from the day
+        try:
+            db.session.delete(food)
+            db.session.commit()
+
+        except Exception as e:
+            return jsonify({"msg": str(e)})
+
+            
+        return jsonify({"msg": "Success!"}), 200
+
+@app.route('/diary/food/all', methods=['GET'])
+def getAllFoods():
+    food = Food.query.all()
+    return jsonify({"foods": list(map(lambda x: x.serialize(),food))}), 200
+
+@app.route('/diary/<int:day_id>', methods=['GET'])
+def getDay(day_id):
+    day = Day.query.filter_by(id=day_id).first()
+    return jsonify({"day": day.serialize()}), 200
 
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -11,7 +11,7 @@ class User(db.Model):
     email = db.Column(db.String(120), unique=True, nullable=False)
     tos = db.Column(db.Boolean(), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
-    diary = db.relationship('Food', backref = 'user')
+    diary = db.relationship('Day', backref = 'user')
     # Diary (relationship 1 to n -> Food)
 
 
@@ -24,7 +24,6 @@ class User(db.Model):
             "email": self.email,
             "first_name": self.firstname,
             "last_name": self.lastname
-
             # do not serialize the password, its a security breach
         }
 
@@ -34,15 +33,45 @@ class User(db.Model):
 
         return True
 
+class Day(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.String(100), nullable=False)
+    foods = db.relationship('Food', backref = 'day')
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def repr(self):
+        return '<Journal Entry for %r>' % self.date
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "date": self.date,
+            "foods": [f.serialize() for f in self.foods]
+        }
+
 class Food(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), nullable=False)
-    calories = db.Column(db.String(50), nullable=False)
-    serving_size = db.Column(db.String(120), unique=True, nullable=False)
-    quantity = db.Column(db.String(80), unique=False, nullable=False)
-    date = db.Column(db.String(50), nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    calories = db.Column(db.String(15), unique=False, nullable=False)
+    serving_unit = db.Column(db.String(50), unique=False, nullable=False)
+    serving_size = db.Column(db.String(120), unique=False, nullable=False)
+    quantity = db.Column(db.String(10), unique=False, nullable=False)
+    day_id = db.Column(db.Integer, db.ForeignKey('day.id'))
     time_of_day = db.Column(db.Enum('morning','afternoon','night'), nullable=False, server_default='morning')
+
+    def repr(self):
+        return '<Food Item %r>' % self.name
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "calories": self.calories,
+            "serving_size": self.serving_size,
+            "quantity": self.quantity,
+            "time_of_day": self.time_of_day,
+            "day_id": self.day_id
+        }
 
 # Date (datetime stamp)
 # Time of day (ENUM morning | afternoon | evening)
